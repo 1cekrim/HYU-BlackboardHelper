9112024b7f13b0ad5388a54434ce85ea1527aa54
1cekrim <1cekrim.dev@gmail.com>
2020-09-20 22:30:56 +0900

    코드 정리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-/*global chrome*/
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom'
-import { Button, Table } from 'react-bootstrap'
-import Moment from 'react-moment';
-import './App.css';
-
+/* global chrome */
+import React from "react";
+import ReactDOM from "react-dom";
+import { Table } from "react-bootstrap";
+import Moment from "react-moment";
+import "./App.css";
 
 function App() {
   return (
@@ -21,12 +20,17 @@ function Loading() {
   const element = (
     <div className="center-main">
       <div>
-        <div className="spinner-grow text-light width: 3rem; height: 3rem;" role="status" />
+        <div
+          className="spinner-grow text-light width: 3rem; height: 3rem;"
+          role="status"
+        />
         <p className="text-light">온라인 출석 조회 불러오는 중...</p>
       </div>
-      <p className="text-success" id="term">최근 학기 불러오는 중...</p>
+      <p className="text-success" id="term">
+        최근 학기 불러오는 중...
+      </p>
     </div>
-  )
+  );
   return element;
 }
 
@@ -38,53 +42,50 @@ async function loadTables() {
       <div class="alert alert-warning" role="alert">
         {err.message}
       </div>
-    )
-    ReactDOM.render(errorElement, document.querySelector('main'));
+    );
+    ReactDOM.render(errorElement, document.querySelector("main"));
   }
 }
 
 async function UpdateAttendanceTables() {
   const userKey = await GetUserKey();
   const coursesData = await GetCourses(userKey);
-  const courses = coursesData['courses'];
-  let termList = coursesData['termList'];
+  const courses = coursesData["courses"];
+  let termList = coursesData["termList"];
   // 내림차순 정렬
   // TODO:  /learn/api/v1/terms API 사용
   termList.sort((a, b) => {
     return Number(b.replace(/[^0-9]/g, "")) - Number(a.replace(/[^0-9]/g, ""));
-  })
+  });
   const targetTerm = termList[0];
-  const targetTermElement = (
-    <div class="target-term">
-      {targetTerm}
-    </div>
-  )
-  ReactDOM.render(targetTermElement, document.querySelector('#term'));
-  const targetCourses = courses.filter(course => course.term === targetTerm);
+  const targetTermElement = <div class="target-term">{targetTerm}</div>;
+  ReactDOM.render(targetTermElement, document.querySelector("#term"));
+  const targetCourses = courses.filter((course) => course.term === targetTerm);
 
-  let courseTables = []
+  let courseTables = [];
   for (const course of targetCourses) {
-    console.log(`${course['name']} 온라인 출석 조회...`);
-    const courseAttendance = await GetCourseAttendance(course['id'], course['name']);
+    console.log(`${course["name"]} 온라인 출석 조회...`);
+    const courseAttendance = await GetCourseAttendance(
+      course["id"],
+      course["name"]
+    );
     // const courseStructure = await GetCourseStructure(course['id'], course['name']);
-    const courseGrades = await GetGrades(course['id'], userKey);
+    const courseGrades = await GetGrades(course["id"], userKey);
     console.log(courseGrades);
     console.log(courseAttendance);
     courseTables.push([courseAttendance, courseGrades]);
   }
   console.log(courseTables);
-  const element = (
-    <DrawAllTable data={courseTables}/>
-  );
-  document.querySelector('main').classList.remove('h-100');
-  ReactDOM.render(element, document.querySelector('main'));
+  const element = <DrawAllTable data={courseTables} />;
+  document.querySelector("main").classList.remove("h-100");
+  ReactDOM.render(element, document.querySelector("main"));
 
   let names = document.getElementsByClassName("grade-name");
   for (let tag of names) {
     tag.addEventListener("click", () => {
-      console.log(tag.getAttribute('link'));
-      const link = tag.getAttribute('link');
-      chrome.windows.create({url: link, type: 'normal'});
+      console.log(tag.getAttribute("link"));
+      const link = tag.getAttribute("link");
+      chrome.windows.create({ url: link, type: "normal" });
     });
   }
 
@@ -94,7 +95,7 @@ async function UpdateAttendanceTables() {
 async function GetResponse(url) {
   return await fetch(url, {
     credentials: "same-origin",
-    mode: "no-cors"
+    mode: "no-cors",
   });
 }
 
@@ -106,15 +107,15 @@ async function GetCourseAttendance(id, name) {
     const html = await rep.text();
     let parser = new DOMParser();
     const doc = parser.parseFromString(html, "text/html");
-    return doc.getElementById('bltiLaunchForm');
+    return doc.getElementById("bltiLaunchForm");
   })();
-  
-  const url = formElement.getAttribute('action');
+
+  const url = formElement.getAttribute("action");
   let data = new URLSearchParams();
   for (const pair of new FormData(formElement)) {
-      data.append(pair[0], pair[1]);
+    data.append(pair[0], pair[1]);
   }
-  data.append('showAll', 'true');
+  data.append("showAll", "true");
 
   const rep = await fetch(url, {
     credentials: "same-origin",
@@ -128,18 +129,20 @@ async function GetCourseAttendance(id, name) {
   const doc = parser.parseFromString(html, "text/html");
   const ele = doc.getElementById("listContainer_databody");
   let courseAttendance = {
-    "id": id,
-    "name": name,
-    "data": []
-  }
+    id: id,
+    name: name,
+    data: [],
+  };
   if (ele) {
-    Array.from(ele.rows).forEach(child => {
-      let data = []
-      Array.from(child.getElementsByTagName('td')).forEach(td => {
-        data.push(td.getElementsByClassName('table-data-cell-value')[0].innerText)
-      })
+    Array.from(ele.rows).forEach((child) => {
+      let data = [];
+      Array.from(child.getElementsByTagName("td")).forEach((td) => {
+        data.push(
+          td.getElementsByClassName("table-data-cell-value")[0].innerText
+        );
+      });
       courseAttendance["data"].push([data[0], data[1], data[6]]);
-    })
+    });
   }
   return courseAttendance;
 }
@@ -150,22 +153,22 @@ async function GetCourses(userKey) {
   let result = [];
   let termList = [];
   const json = await rep.json();
-  json['results'].forEach(course => {
-    course = course['course'];
+  json["results"].forEach((course) => {
+    course = course["course"];
     const data = {
-      'name': course['name'],
-      'id': course['id'],
-      'courseId': course['courseId'],
-      'term': 'term' in course ? course['term']['name'] : null
-    }
-    if (data['term'] && !termList.includes(data['term'])) {
-      termList.push(data['term']);
+      name: course["name"],
+      id: course["id"],
+      courseId: course["courseId"],
+      term: "term" in course ? course["term"]["name"] : null,
+    };
+    if (data["term"] && !termList.includes(data["term"])) {
+      termList.push(data["term"]);
     }
     result.push(data);
   });
   return {
-    "courses": result,
-    "termList": termList
+    courses: result,
+    termList: termList,
   };
 }
 
@@ -173,73 +176,65 @@ async function GetUserKey() {
   const url = "https://learn.hanyang.ac.kr/learn/api/v1/users/me";
   const rep = await GetResponse(url);
   const json = await rep.json();
-  if ('id' in json) {
-    return json['id'];
+  if ("id" in json) {
+    return json["id"];
   }
   console.error(json);
   throw new Error("사용자 ID를 찾을 수 없습니다.");
-  return null;
 }
 
 async function GetCourseStructure(id, name) {
   async function GetChildren(rootId) {
-    const url = `https://learn.hanyang.ac.kr/learn/api/v1/courses/${id}/contents/${rootId}/children?@view=Summary&expand=assignedGroups,selfEnrollmentGroups.group,gradebookCategory&limit=10000`
+    const url = `https://learn.hanyang.ac.kr/learn/api/v1/courses/${id}/contents/${rootId}/children?@view=Summary&expand=assignedGroups,selfEnrollmentGroups.group,gradebookCategory&limit=10000`;
     const rep = await GetResponse(url);
-    const contents = (await rep.json())['results']
+    const contents = (await rep.json())["results"];
     return contents;
   }
-  let st = []
-  let root = ['ROOT', {
-    'name': 'ROOT',
-    'children': [],
-    'contents': [],
-    'contentDetail': ''
-  }]
+  let st = [];
+  let root = [
+    "ROOT",
+    {
+      name: "ROOT",
+      children: [],
+      contents: [],
+      contentDetail: "",
+    },
+  ];
   st.push(root);
 
   while (st.length > 0) {
     let [id, parent] = st.pop();
     const children = await GetChildren(id);
-    children.forEach(now => {
-      if (!('contentDetail' in now)) {
+    children.forEach((now) => {
+      if (!("contentDetail" in now)) {
         console.error(`contentDetail does not exist\n${now}`);
         return;
       }
 
       let content = {
-        'name': now['title'],
-        'children': [],
-        'contents': [],
-        'contentDetail': now['contentDetail']
+        name: now["title"],
+        children: [],
+        contents: [],
+        contentDetail: now["contentDetail"],
       };
 
       //"resource/x-bb-lesson" 학생용 가이드
       //"resource/x-bb-file" 다운로드 가능한 파일
       //"resource/x-bb-asmt-test-link" 과제
-      // genericReadOnlyData->dueDate: 과제 마감 기한
-      // 9시간 더해야 함!!!!
-      //  '' ->hasGradeColumn: gradingColumn유무?
-      // 과제->test->assessment->gradingColumn
-        // columnName: 과제명
-        // dueDate: 과제 마감 기한
-      //  /learn/api/v1/courses/_38929_1/gradebook/columns/_332121_1/grades/_2868288_1/attempts?fields=id,status,attemptDate 
-      // _332121_1: gradingColumn.gradebookCategory.id
-      // {"results":[{"attemptDate":"2020-09-19T05:09:06.856Z","id":"_2289349_1","status":"IN_PROGRESS"}],"paging":{"previousPage":"","nextPage":"","count":1,"limit":1000,"offset":0},"permissions":{"createAttempt":false,"deleteAttempt":false,"viewAttempt":true,"editAttempt":false}}
-
       // 폴더
-      if ('resource/x-bb-folder' == now['contentHandler'] && !now['contentDetail']['resource/x-bb-folder']['isBbPage'] && now['contentDetail']['resource/x-bb-folder']['isFolder']) {
-        st.push([now['id'], content]);
+      if (
+        "resource/x-bb-folder" === now["contentHandler"] &&
+        !now["contentDetail"]["resource/x-bb-folder"]["isBbPage"] &&
+        now["contentDetail"]["resource/x-bb-folder"]["isFolder"]
+      ) {
+        st.push([now["id"], content]);
         console.log(st);
-        parent['children'].push(content);
-      }
-      // else if ('resource/x-bb-file' in now['contentDetail']) {
-
-      // }
-      else {
+        parent["children"].push(content);
+      } else {
         // 파일, 영상, 비디오는 따로 구현하지 않음
         // TODO: 다운로드 기능 필요해지면 그때 구현할 것
         // 일단 다 넣음
-        parent['contents'].push(now);
+        parent["contents"].push(now);
       }
     });
   }
@@ -253,7 +248,7 @@ async function GetGrades(id, userId) {
     // expand=attemptsLeft
     const rep = await GetResponse(url);
     const json = await rep.json();
-    const results = json['results'];
+    const results = json["results"];
     return results;
   }
 
@@ -262,7 +257,7 @@ async function GetGrades(id, userId) {
     // expand=attemptsLeft
     const rep = await GetResponse(url);
     const json = await rep.json();
-    const results = json['results'];
+    const results = json["results"];
     return results;
   }
 
@@ -271,92 +266,84 @@ async function GetGrades(id, userId) {
     const json = await rep.json();
     return json;
   }
-  
+
   const grades = await GetGradeBookGrades();
   const columns = await GetGradeBookColumns();
 
   let result = [];
 
   for (let column of columns) {
-    const name = column['columnName'];
-    if (name == '출석') {
+    const name = column["columnName"];
+    if (name === "출석") {
       continue;
     }
-    const dueDate = column['dueDate']; // 9시간 추가
-    const columnId = column['id'];
-    let lastAttemptId = ''
-    let lastAttemptUrl = ''
+    const dueDate = column["dueDate"]; // 9시간 추가
+    const columnId = column["id"];
+    let lastAttemptId = "";
+    let lastAttemptUrl = "";
     for (const grade of grades) {
-      if (grade['columnId'] == columnId) {
-        lastAttemptId = grade['lastAttemptId'];
-        lastAttemptUrl = grade['lastAttemptUrl'];
+      if (grade["columnId"] === columnId) {
+        lastAttemptId = grade["lastAttemptId"];
+        lastAttemptUrl = grade["lastAttemptUrl"];
       }
     }
-    if (lastAttemptId == '') {
+    if (lastAttemptId === "") {
       // TODO: 예외 처리
       console.error(column);
     }
 
     const attemptData = await GetAttemptData(lastAttemptUrl);
-    // NOT_ATTEMPTED (deprecated)
-    // ABANDONED (deprecated)
-    // IN_PROGRESS
-    // SUSPENDED
-    // CANCELLED (deprecated)
-    // NEEDS_GRADING
-    // COMPLETED
-    // IN_MORE_PROGRESS
-    // NEEDS_MORE_GRADING
+
     const status = {
-      'NOT_ATTEMPTED': {
-        'name': '시도하지 않음',
-        'grade': false
+      NOT_ATTEMPTED: {
+        name: "시도하지 않음",
+        grade: false,
       },
-      'ABANDONED': {
-        'name': '포기함',
-        'grade': false
+      ABANDONED: {
+        name: "포기함",
+        grade: false,
       },
-      'IN_PROGRESS': {
-        'name': '진행중',
-        'grade': false
+      IN_PROGRESS: {
+        name: "진행중",
+        grade: false,
       },
-      'SUSPENDED': {
-        'name': '유보됨',
-        'grade': false
+      SUSPENDED: {
+        name: "유보됨",
+        grade: false,
       },
-      'CANCELLED': {
-        'name': '취소됨',
-        'grade': false
+      CANCELLED: {
+        name: "취소됨",
+        grade: false,
       },
-      'NEEDS_GRADING': {
-        'name': '채점중',
-        'grade': true
+      NEEDS_GRADING: {
+        name: "채점중",
+        grade: true,
       },
-      'COMPLETED': {
-        'name': '완료됨',
-        'grade': true
+      COMPLETED: {
+        name: "완료됨",
+        grade: true,
       },
-      'IN_MORE_PROGRESS': {
-        'name': '추가 진행 필요',
-        'grade': false
+      IN_MORE_PROGRESS: {
+        name: "추가 진행 필요",
+        grade: false,
       },
-      'NEEDS_MORE_GRADING': {
-        'name': '추가 채점 필요',
-        'grade': false
-      }
-    }
+      NEEDS_MORE_GRADING: {
+        name: "추가 채점 필요",
+        grade: false,
+      },
+    };
 
     result.push({
-      'name': name,
-      'dueDate': dueDate,
-      'columnId': columnId,
-      'lastAttemptid': lastAttemptId,
-      'lastAttemptUrl': '',
-      'attempt': attemptData,
-      'contentId': column['contentId'],
-      'contentUrl': `https://learn.hanyang.ac.kr/ultra/courses/${id}/outline/assessment/${column['contentId']}/overview?courseId=${id}`,
-      'status': status[attemptData['status']]
-    })
+      name: name,
+      dueDate: dueDate,
+      columnId: columnId,
+      lastAttemptid: lastAttemptId,
+      lastAttemptUrl: "",
+      attempt: attemptData,
+      contentId: column["contentId"],
+      contentUrl: `https://learn.hanyang.ac.kr/ultra/courses/${id}/outline/assessment/${column["contentId"]}/overview?courseId=${id}`,
+      status: status[attemptData["status"]],
+    });
   }
 
   return result;
@@ -366,7 +353,12 @@ function DrawAllTable(probs) {
   return (
     <div>
       {probs.data.map((course, _) => (
-        <AttendanceTable title={course[0].name} data={course[0].data} grade={course[1]} key={course.id}/>
+        <AttendanceTable
+          title={course[0].name}
+          data={course[0].data}
+          grade={course[1]}
+          key={course.id}
+        />
       ))}
     </div>
   );
@@ -376,19 +368,15 @@ function AttendanceTable(probs) {
   return (
     <div>
       <p className="text-white Table-title">{probs.title}</p>
-      <AttendanceTableContent data={probs.data}/>
-      <GradeTableContent data={probs.grade}/>
+      <AttendanceTableContent data={probs.data} />
+      <GradeTableContent data={probs.grade} />
     </div>
   );
 }
 
 function AttendanceTableContent(probs) {
-  if (probs.data.length == 0) {
-    return (
-      <div className="text-white">
-        강의 영상이 없습니다
-      </div>
-    );
+  if (probs.data.length === 0) {
+    return <div className="text-white">강의 영상이 없습니다</div>;
   }
   return (
     <Table responsive="lg" size="sm" variant="dark" bordered>
@@ -402,8 +390,13 @@ function AttendanceTableContent(probs) {
       <tbody>
         {probs.data.map((row, idx) => (
           <tr key={idx}>
-            {row.map((ele, idx) => 
-              (<td key={idx} className={row[2]==='F' ? 'text-warning' : 'text-success'}>{ele}</td>
+            {row.map((ele, idx) => (
+              <td
+                key={idx}
+                className={row[2] === "F" ? "text-warning" : "text-success"}
+              >
+                {ele}
+              </td>
             ))}
           </tr>
         ))}
@@ -413,10 +406,8 @@ function AttendanceTableContent(probs) {
 }
 
 function GradeTableContent(probs) {
-  if (probs.data.length == 0) {
-    return (
-      <div></div>
-    );
+  if (probs.data.length === 0) {
+    return <div></div>;
   }
   return (
     <Table responsive="lg" size="sm" variant="dark" bordered>
@@ -429,23 +420,24 @@ function GradeTableContent(probs) {
       </thead>
       <tbody>
         {probs.data.map((row, idx) => (
-          <tr key={idx} className={row['status']['grade'] ? 'text-success' : 'text-warning'}>
-            <td className='grade-name' link={row['contentUrl']}>
-              {row['name']}
+          <tr
+            key={idx}
+            className={row["status"]["grade"] ? "text-success" : "text-warning"}
+          >
+            <td className="grade-name" link={row["contentUrl"]}>
+              {row["name"]}
             </td>
             <td>
-              <Moment local format='yyyy-MM-DD HH:mm'>
-                {row['dueDate']}
+              <Moment local format="yyyy-MM-DD HH:mm">
+                {row["dueDate"]}
               </Moment>
             </td>
-            <td>
-              {row['status']['name']}
-            </td>
+            <td>{row["status"]["name"]}</td>
           </tr>
         ))}
       </tbody>
     </Table>
-  )
+  );
 }
 
 export default App;
